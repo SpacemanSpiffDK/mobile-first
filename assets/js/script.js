054dcf38608dcdb097fd7247d32e2f93933ac0b4
Dan Høegh <dan.hoegh@gmail.com>
2019-12-03 10:01:56 +0100

    Bug-fixing and JS refactoring
    Fixed a bug where the mobilemenu is visible when changing viewport size and on reload.
    Refactored JS to use queryselect and variables for commonly used elements
    Removed #body-id from <body>, since it can be selected directly through queryselect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 // JS by Dan Høegh
 // UCN MMD 2019
 
+// setup variables for things that are used more than once
+let elmBody = document.querySelector('body');
+const strActive = 'nav-active';
 
 // Add class "nav-active" to <body> when #menu-btn is clicked
-document.getElementById("menu-btn").addEventListener("click", function(event){
-	event.preventDefault(); // stops the element from doing it's usual stuff
-	document.getElementById("body-id").classList.add("nav-active");
+document.querySelector('#menu-btn').addEventListener('click', function(event){
+	event.preventDefault(); // stops the element from doing its usual stuff
+	elmBody.classList.add(strActive); // add the content of strActive to the class attribute on <body>
 });
 
 // remove class "nav-active" from <body> when #menu-btn-close is clicked
-document.getElementById("menu-btn-close").addEventListener("click", function(event){
-	event.preventDefault(); // stops the element from doing it's usual stuff
-	document.getElementById("body-id").classList.remove("nav-active");
+document.querySelector('#menu-btn-close').addEventListener('click', function(event){
+	event.preventDefault(); // stops the element from doing its usual stuff
+	elmBody.classList.remove(strActive); // remove the content of strActive to the class attribute on <body>
 });
